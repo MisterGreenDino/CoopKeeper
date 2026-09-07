48148e6d3ce32a2a5c212b3a5102140bebef94ee
matteo87240 <84675434+matteo87240@users.noreply.github.com>
2026-09-07 21:45:04 +0200

    Add files via upload

--- src/main/java/org/bot/electrionTracker/Candidate.java
+++ src/main/java/org/bot/electrionTracker/Candidate.java
@@ -1,4 +1,19 @@
 package org.bot.electrionTracker;
 
+import java.util.List;
+
+/** One candidate running in a SkyBlock mayoral election. */
 public class Candidate {
+
+    public final String key;
+    public final String name;
+    public final List<String> perks;
+    public final int votes;
+
+    public Candidate(String key, String name, List<String> perks, int votes) {
+        this.key = key;
+        this.name = name;
+        this.perks = perks;
+        this.votes = votes;
+    }
 }
